9805af22df6b7d506cf728ac274149087c3881fd
Damian <damianczapiewski@gmail.com>
2024-01-06 22:35:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+(function () {
+ function alter(arr, processor) {
+  const copy = [...arr];
+	 processor(copy);
+	 return copy;
+ }
+ 
+ const methods = [
+  'push', 'pop',
+  'unshift', 'shift',
+  'splice', 'sort', 'reverse'
+ ].reduce((api, method) => ({
+		...api,
+		[method](...args) {
+		 return alter(
+		  this,
+		  arr => arr[method](...args)
+		 )}
+		}), {});
+
+ Object.defineProperty(Array.prototype, 'im', {
+	 get() {
+		 return new Proxy(methods, {
+		 	get: (api, method, ...rest) => {
+		 		if (method in api) {
+		 			return api[method].bind(this);
+		 		}
+		 		return Reflect.get(
+		 			api, method, ...rest
+		 		);
+		 	}
+		 });
+	 }
+ });
+})()
